a7e2de91e1722dcfdd76ee31c6ba5944cae20eea
huynhanx03 <huynhnhandanang@gmail.com>
2024-12-01 21:39:32 +0700

    test

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-import Helper.firebase
-import Recommended_System.recommend
+# import Helper.firebase
+# import Recommended_System.recommend
+
+# productsRC = Helper.firebase.GetProductRC()
+# RS = Recommended_System.recommend.HybridRecommender(productsRC, k = 10)
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS 
 
-productsRC = Helper.firebase.GetProductRC()
-RS = Recommended_System.recommend.HybridRecommender(productsRC, k = 10)
-
 app = Flask(__name__)
 CORS(app)  
 
-@app.route('/recommend', methods=['POST'])
-def recommendHTTP():
-    try:
-        data = request.get_json()
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
 
-        product_id = data['MaSanPham']
-        user_id = data['MaKhachHang']
+# @app.route('/recommend', methods=['POST'])
+# def recommendHTTP():
+#     try:
+#         data = request.get_json()
 
-        productRecommend = RS.recommend(user_id, product_id, True, True)
+#         product_id = data['MaSanPham']
+#         user_id = data['MaKhachHang']
 
-        return jsonify(productRecommend)
+#         productRecommend = RS.recommend(user_id, product_id, True, True)
+
+#         return jsonify(productRecommend)
     
-    except Exception as e:
-        return jsonify({"error": str(e)})
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run()
