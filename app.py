862337aaf927a33dfead2e6ed7e92ef6e391c862
huynhanx03 <huynhnhandanang@gmail.com>
2024-12-12 08:38:18 +0700

    final

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
-import tensorflow as tf
-from tensorflow.keras import mixed_precision
-tf.config.intra_op_parallelism_threads = 4  # Giới hạn số lượng thread CPU sử dụng
-tf.config.inter_op_parallelism_threads = 2  # Điều chỉnh số lượng thread giữa các phép toán
-tf.config.optimizer.set_jit(True)  # Bật JIT (XLA)
-
-policy = mixed_precision.Policy('mixed_float16')
-mixed_precision.set_global_policy(policy)
-
-# import Recommended_System.recommend
+import Recommended_System.recommend
 import Chatbot.chatbot
 
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS 
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
 
-# app = Flask(__name__)
-# CORS(app)  
+app = Flask(__name__)
+CORS(app)  
 
-# @app.route('/')
-# def hello_world():
-# 	return 'Hello World!'
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
 
-# @app.route('/recommend', methods=['POST'])
-# def recommendHTTP():
-#     try:
-#         data = request.get_json()
+@app.route('/recommend', methods=['POST'])
+def recommendHTTP():
+    try:
+        data = request.get_json()
 
-#         product_id = data['MaSanPham']
-#         user_id = data['MaKhachHang']
+        product_id = data['MaSanPham']
+        user_id = data['MaKhachHang']
 
-#         productRecommend = RS.recommend(user_id, product_id, True, True)
+        productRecommend = Recommended_System.recommend.RS.recommend(user_id, product_id, True, True)
 
-#         return jsonify(productRecommend)
+        return jsonify(productRecommend)
     
-#     except Exception as e:
-#         return jsonify({"error": str(e)})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
-print(Chatbot.chatbot.chatBotGetMessage("tôi nên dùng cà phê nào", "KH0001"))
+@app.route('/chatbot', methods=['POST'])
+def chatbotHTTP():
+    try:
+        data = request.get_json()
 
-# if __name__ == "__main__":
-#     app.run()
+        question = data['CauHoi']
+        user_id = data['MaKhachHang']
+
+        resp = Chatbot.chatbot.chatBotGetMessage(question, user_id)
+
+        return jsonify(resp)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run()
